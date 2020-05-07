e82acbd2409d383f9cb3f1697f25462ee021bd79
niuniu13453581675 <2670709623@qq.com>
2020-05-07 17:29:46 +0800

    分类更新

--- src/views/work/commodity1/administration.vue
+++ src/views/work/commodity1/administration.vue
@@ -2,17 +2,65 @@
 <!--    管理分类-->
   <div>
     <a-card title="分类管理" >
-      <a-table :columns="columns" :data-source="data"  >
-        <a slot="sort" slot-scope="text" href="javascript:;">Delete</a>
-        <a slot="category" slot-scope="text" href="javascript:;">Delete</a>
-        <a slot="bianji" slot-scope="text" href="javascript:;">Delete</a>
+      <a-button type="primary" class="modifyBtn1" @click="deleteAllCategories()">批量删除</a-button>
+      <a-table :columns="columns" :data-source="data" :row-selection="{  onChange: onSelectChange }" >
+        <span slot="sort" slot-scope="text,record" >
+            <a @click="addCategories(record)">排序</a>
+        </span>
+        <span slot="category" slot-scope="text,record">
+          <a @click="addCategories(record)">添加子分类</a>
+        </span>
+        <span slot="bianji" slot-scope="text,record">
+          <a-button type="primary" class="modifyBtn" @click="modifyCategories(record)">修改</a-button>
+          <a-button type="primary" @click="deleteCategories(record)">删除</a-button>
+        </span>
       </a-table>
     </a-card>
+
+<!--    添加子分类弹出框-->
+    <a-modal
+      title="添加子分类"
+      :visible="addvisible"
+      :confirm-loading="confirmLoading"
+      @ok="handleOk"
+      @cancel="handleCancel"
+    >
+      <a-form-model :model="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 12 }">
+        <a-form-model-item label="分类名">
+          <a-input v-model="form.name" />
+        </a-form-model-item>
+        <a-form-model-item label="是否是父级分类">
+          <a-input v-model="form.isParent"  placeholder="1代表是，0表示否" />
+        </a-form-model-item>
+        <a-form-model-item label="排序">
+          <a-input v-model="form.sort"  />
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+
+    <!--    修改分类弹出框-->
+    <a-modal
+      title="修改分类"
+      :visible="modifyvisible"
+      :confirm-loading="confirmLoading"
+      @ok="modifyhandleOk"
+      @cancel="modifyhandleCancel"
+    >
+      <a-form-model :model="form" :label-col="{ span: 6 }" :wrapper-col="{ span: 12 }">
+        <a-form-model-item label="分类名">
+          <a-input v-model="modifyform.name" />
+        </a-form-model-item>
+      </a-form-model>
+    </a-modal>
+
   </div>
 </template>
 
 <script>
   import {getAction} from '../../../api/manage'
+  import {postAction} from '../../../api/manage'
+  import {deleteAction} from '../../../api/manage'
+
 
   export default {
         data() {
@@ -23,13 +71,158 @@
                 { title: '添加子分类', dataIndex: 'id', key: 'id', scopedSlots: { customRender: 'category' } },
                 { title: '编辑', dataIndex: 'isflag', key: 'isflag', scopedSlots: { customRender: 'bianji' } },
               ],
-              data:[
-
-              ]
+              data:[],
+              addvisible: false, //控制添加弹出框
+              modifyvisible: false, //控制添加弹出框
+              confirmLoading: false,
+              form:{
+                name:'',
+                isParent:'',
+                sort:''
+              },
+              pid:'',  //点击添加子分类获取到的父级
+              modifyform:{
+                name:''
+              },
+              ids:[]
             };
         },
         methods: {
+          onSelectChange(selectedRowKeys, selectedRows) {
+            // console.log(selectedRows)
+            let that=this
+            selectedRows.forEach(e=>{
+              that.ids.push(e.id)
+            })
+            console.log(1,selectedRows)
+          },
+          // 点击批量删除按钮
+          deleteAllCategories(){
+            let that=this
+            this.$confirm({
+              title: '确定要删除该分类吗?',
+              content: '该分类包含多个子分类',
+              okText: '确定',
+              okType: 'primary',
+              cancelText: '取消',
+              onOk() {
+                let param = new URLSearchParams()
+                param.append('ids',that.ids)
+                deleteAction('/kunze/category/categoryDels',param).then((res)=>{
+                  console.log(res)
+                  if(res.success==true){
+                    that.$message.success(
+                      '删除成功'
+                    );
+                    that.getAllCategories()
+                  }
+                })
+              },
+              onCancel() {
 
+              },
+            });
+          },
+          //点击删除按钮
+          deleteCategories(e){
+            let id=e.id
+            this.$confirm({
+              title: '确定要删除该分类吗?',
+              content: '该分类包含多个子分类',
+              okText: '确定',
+              okType: 'primary',
+              cancelText: '取消',
+              onOk() {
+                deleteAction('/kunze/category/categoryDel',{id:id}).then((res)=>{
+                  if(res.success==true){
+                    this.$message.success(
+                      '删除成功'
+                    );
+                    this.getAllCategories()
+                  }
+                })
+              },
+              onCancel() {
+
+              },
+            });
+          },
+          // 点击修改取消按钮
+          modifyhandleCancel(){
+            this.modifyvisible=false
+            this.modifyform.name=''
+          },
+          //点击修改确认按钮
+          modifyhandleOk(){
+            this.modifyvisible=false
+            let category={
+              id: this.pid,
+              name: this.modifyform.name
+            }
+            postAction('/kunze/category/categoryUpdate',category).then((res)=>{
+              if(res.success){
+                this.$message.success(
+                  '修改成功'
+                );
+                this.getAllCategories()
+              }else {
+                this.$message.error('修改失败');
+                this.modifyform.name=''
+              }
+              this.modifyform.name=''
+            })
+          },
+          // 点击修改按钮
+          modifyCategories(e){
+            this.pid=e.id
+            this.modifyform.name=e.name
+            this.modifyvisible=true
+          },
+          // 点击添加子分类弹窗确认按钮
+          handleOk(){
+            this.addvisible=false
+            let category={
+              isParent: this.form.isParent,
+              name:this.form.name,
+              parentId:this.pid,
+              image: "string",
+              index: "string",
+              isflag: "string",
+              sort:this.form.sort
+            }
+            postAction('/kunze/category/categorySave',category).then((res)=>{
+              if(res.success){
+                this.$message.success(
+                  '添加成功'
+                );
+                this.getAllCategories()
+              }else {
+                this.$message.error('添加失败');
+              }
+              this.form.name=''
+              this.form.sort=''
+              this.form.isParent=''
+            })
+
+          },
+          // 点击添加子分类弹窗取消按钮
+          handleCancel(){
+            this.addvisible=false
+            this.form.name=''
+            this.form.sort=''
+            this.form.isParent=''
+          },
+          // 点击添加子集分类
+          addCategories(e){
+            // console.log(e.isParent)
+            this.pid=e.id
+            if(e.isParent==0){
+              this.$message.warning('底层分类不可添加子分类');
+            }
+            if(e.isParent==1){
+              this.addvisible=true
+            }
+          },
           // 获取全部分类
           getAllCategories(){
             getAction('/kunze/category/qryList',{id:'',pid:''}).then((res)=>{
@@ -38,9 +231,11 @@
               this.data.forEach(e=>{
                 e.key=key++
                 e.children=JSON.parse(JSON.stringify(e.childrenList))
+                e.childrenList=''
                 e.children.forEach(e=>{
                   e.key=key++
                   e.children=JSON.parse(JSON.stringify(e.childrenList))
+                  e.childrenList=''
                   e.children.forEach(e=>{
                     e.key=key++
                   })
@@ -57,5 +252,10 @@
 </script>
 
 <style scoped>
-
+.modifyBtn{
+  margin-right: 10px;
+}
+  .modifyBtn1{
+    margin-bottom: 10px;
+  }
 </style>
--- src/views/work/commodity1/pinpai.vue
+++ src/views/work/commodity1/pinpai.vue
@@ -1,450 +1,56 @@
 <template>
-<!--    商品品牌-->
-  <el-card class="box-card">
-    <div slot="header" class="clearfix">
-      <span>分类管理</span>
-    </div>
-    <div>
-      <el-button type="primary" round class="flgl-btn" @click="dialogFormVisible=true">添加品牌</el-button>
-      <el-button type="primary" round class="flgl-btn" @click="plscpp()">批量删除</el-button>
-
-      <el-table
-        :data="tableData"
-        border
-        style="width: 100%"
-        @selection-change="handleSelectionChange">
-        <el-table-column
-          type="selection"
-          width="55">
-        </el-table-column>
-        <el-table-column
-          label="图片">
-          <template slot-scope="scope">
-            <img :src=scope.row.image alt="">
-          </template>
-        </el-table-column>
-
-        <el-table-column
-          prop="letter"
-          label="商标">
-        </el-table-column>
-        <el-table-column
-          prop="updateName"
-          label="管理员">
-        </el-table-column>
-        <el-table-column
-          prop="kid"
-          label="kid">
-        </el-table-column>
-        <el-table-column
-          prop="bid"
-          label="bid">
-        </el-table-column>
-        <el-table-column
-          prop="kname"
-          label="商品名称">
-        </el-table-column>
-        <el-table-column
-          prop="bname"
-          label="品牌名称">
-        </el-table-column>
-        <el-table-column
-          fixed="right"
-          label="操作">
-          <template slot-scope="scope">
-            <el-button  type="text" size="small" @click="xiupp(scope.row)">修改</el-button>
-            <el-button type="text" size="small" @click="ppdel(scope.row)">删除</el-button>
-          </template>
-        </el-table-column>
-      </el-table>
+  <!--    商品品牌-->
+  <div>
+    <a-card title="品牌管理" >
+      <a-button type="primary" class="modifyBtn1" @click="deleteAllCategories()">批量删除</a-button>
+      <a-table :columns="columns" :data-source="data" :row-selection="{  onChange: onSelectChange }" >
+        <span slot="bianji" slot-scope="text,record">
+          <a-button type="primary" class="modifyBtn" @click="modifyCategories(record)">修改</a-button>
+          <a-button type="primary" @click="deleteCategories(record)">删除</a-button>
+        </span>
+      </a-table>
+    </a-card>
 
 
-
-    </div>
-<!--    分页-->
-    <div class="block">
-      <span class="demonstration">调整每页显示条数</span>
-      <el-pagination
-        @size-change="handleSizeChange"
-        @current-change="handleCurrentChange"
-        :current-page.sync="currentPage2"
-        :page-sizes="[10, 20, 30, 40]"
-        :page-size="100"
-        layout="sizes, prev, pager, next"
-        :total=total>
-      </el-pagination>
-    </div>
-
-
-<!--    添加品牌-->
-    <el-dialog title="添加品牌" :visible.sync="dialogFormVisible">
-      <el-form :model="form">
-        <el-row :gutter="20">
-          <el-col :span="10">
-            <el-form-item label="品牌名称" :label-width="formLabelWidth">
-              <el-input v-model="form.name" autocomplete="off" ></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="10">
-            <el-form-item label="商标" :label-width="formLabelWidth">
-              <el-input v-model="form.letter" autocomplete="off" ></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="10">
-            <el-form-item label="KID" :label-width="formLabelWidth">
-              <el-input v-model="form.kid" autocomplete="off" ></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="10">
-            <el-form-item label="图片" :label-width="formLabelWidth">
-              <el-upload
-                class="avatar-uploader"
-                action="https://jsonplaceholder.typicode.com/posts/"
-                :show-file-list="false"
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload">
-                <img v-if="imageUrl" :src="imageUrl" class="avatar">
-                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-              </el-upload>
-            </el-form-item>
-          </el-col>
-        </el-row>
-
-
-
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="qxtj()">取 消</el-button>
-        <el-button type="primary" @click="addpp()">确 定</el-button>
-      </div>
-    </el-dialog>
-
-<!--    修改品牌-->
-    <!--    添加分类-->
-    <el-dialog title="修改品牌" :visible.sync="dialogFormVisible1">
-      <el-form :model="xgform">
-        <el-row :gutter="20">
-          <el-col :span="10">
-            <el-form-item label="品牌名称" :label-width="formLabelWidth">
-              <el-input v-model="xgform.name" autocomplete="off" ></el-input>
-            </el-form-item>
-          </el-col>
-          <el-col :span="10">
-            <el-form-item label="商标" :label-width="formLabelWidth">
-              <el-input v-model="xgform.letter" autocomplete="off" ></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="10">
-            <el-form-item label="KID" :label-width="formLabelWidth">
-              <el-input v-model="xgform.kid" autocomplete="off" ></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
-        <el-row :gutter="20">
-          <el-col :span="10">
-            <el-form-item label="图片" :label-width="formLabelWidth">
-              <el-upload
-                class="avatar-uploader"
-                action="https://jsonplaceholder.typicode.com/posts/"
-                :show-file-list="false"
-                :on-success="handleAvatarSuccess"
-                :before-upload="beforeAvatarUpload">
-                <img v-if="imageUrl" :src="imageUrl" class="avatar">
-                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
-              </el-upload>
-            </el-form-item>
-          </el-col>
-        </el-row>
-
-
-
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="qxxg()">取 消</el-button>
-        <el-button type="primary" @click="qdxg()">确 定</el-button>
-      </div>
-    </el-dialog>
-
-
-
-
-  </el-card>
-
+  </div>
 </template>
 
 <script>
   import {getAction} from '../../../api/manage'
-  import {deleteAction} from '../../../api/manage'
   import {postAction} from '../../../api/manage'
+  import {deleteAction} from '../../../api/manage'
+
+
   export default {
-    name: 'pinpai',
-      data(){
-        return{
-            tableData: [],
-            dialogFormVisible:false,
-            dialogFormVisible1:false,
-            form:{
-                name:'' ,
-                letter:'',
-                image:'',
-                kid:''
-            },
-            xgform:{
-                name:'' ,
-                letter:'',
-                image:'',
-                kid:''
-            },
-            formLabelWidth:'120px',
-            imageUrl: '',
-            currentPage2: 1,
-            total:'',
-            pagee:10,
-            bids:[]
-        }
-      },
-      methods:{
-        // 点击修改确认按钮
-          qdxg(){
-              this.xgform.image=this.imageUrl
-              this.xgform.kid=this.imageUrl
-              let nn=JSON.parse(JSON.stringify(this.xgform))
-              postAction('/kunze/brand/updateBrand',{brandVo:nn}).then((res)=>{
+    data() {
+      return {
+        columns:[
+          { title: '分类名称', dataIndex: 'name', key: 'name' },
+          { title: '排序', dataIndex: 'sort', key: 'sort', scopedSlots: { customRender: 'sort' } },
+          { title: '添加子分类', dataIndex: 'id', key: 'id', scopedSlots: { customRender: 'category' } },
+          { title: '编辑', dataIndex: 'isflag', key: 'isflag', scopedSlots: { customRender: 'bianji' } },
+        ],
+        data:[],
+      };
+    },
+    methods: {
 
-              })
-          },
-        // 点击修改取消按钮
-          qxxg(){
-              this.xgform.image=''
-              this.xgform.name=''
-              this.xgform.kid=''
-              this.xgform.letter=''
-              this.dialogFormVisible1=false
-              this.imageUrl=''
-          },
-        // 点击修改按钮
-          xiupp(row){
-              this.dialogFormVisible1=true
-              this.imageUrl=row.image
-              this.xgform.name=row.bname
-              this.xgform.letter=row.letter
-              this.xgform.kid=row.bid
+      // 获取全部分类
+      getAllCategories(){
 
-
-
-          },
-        // 批量删除
-          plscpp(){
-              if(this.bids.length==0){
-                  this.$message.error('请选择要删除的品牌');
-
-              }else {
-                  let nn=this.bids.toString()
-
-                  this.$confirm('是否删除该品牌?', '提示', {
-                      confirmButtonText: '确定',
-                      cancelButtonText: '取消',
-                      type: 'warning'
-                  }).then(() => {
-                      deleteAction('/kunze/brand/delBrands',{bids:nn}).then((res)=>{
-
-                          if(res.code==200){
-                              this.$message({
-                                  type: 'success',
-                                  message: '删除成功!'})
-                              this.int()
-                              this.bids=[]
-                          }else {
-                              this.$message.error('删除失败');
-                          }
-                      }).cache(()=>{
-                          this.$message.error('删除失败');
-
-                      })
-                  })
-              }
-
-          },
-        // 全选
-          handleSelectionChange(val) {
-              this.bids=[]
-
-              val.forEach(e=>{
-                  this.bids.push(e.bid)
-              })
-          },
-        // 分页
-          handleSizeChange(val) {
-              this.pagee=val
-              getAction('/kunze/brand/qryBrandList',{pageSize:val}).then((res)=>{
-                  this.tableData=res.result.list
-
-              })
-          },
-          handleCurrentChange(val) {
-              getAction('/kunze/brand/qryBrandList',{pageSize:this.pagee,pageNo:val}).then((res)=>{
-                  this.tableData=res.result.list
-
-              })
-          },
-        // 点击取消添加按钮
-          qxtj(){
-              this.dialogFormVisible=false
-              this.form.image=''
-              this.form.name=''
-              this.form.kid=''
-              this.form.letter=''
-              this.imageUrl=''
-          },
-        // 点击确定添加品按钮
-          addpp(){
-
-              this.form.image=this.imageUrl
-
-              let brandVo=JSON.parse(JSON.stringify(this.form))
-
-              postAction('/kunze/brand/saveBrand',{brandVo: brandVo}).then((res)=>{
-
-                  if(res.code==200){
-                      this.$message({
-                          type: 'success',
-                          message: '添加成功!'
-                      });
-                      this.form.image=''
-                      this.form.name=''
-                      this.form.kid=''
-                      this.form.letter=''
-                      this.dialogFormVisible=false
-                      this.imageUrl=''
-                  }else {
-                      this.$message.error('添加失败');
-                  }
-
-              }).catch((err)=>{
-                  this.$message.error('添加失败');
-              })
-          },
-        //   图片上传
-          handleAvatarSuccess(res, file) {
-              this.imageUrl = URL.createObjectURL(file.raw);
-
-          },
-          beforeAvatarUpload(file) {
-              const isJPG = file.type === 'image/jpeg';
-              const isLt2M = file.size / 1024 / 1024 < 2;
-
-              if (!isJPG) {
-                  this.$message.error('上传头像图片只能是 JPG 格式!');
-              }
-              if (!isLt2M) {
-                  this.$message.error('上传头像图片大小不能超过 2MB!');
-              }
-              return isJPG && isLt2M;
-          },
-
-        // 删除单个
-          ppdel(row){
-              this.$confirm('是否删除该品牌?', '提示', {
-                  confirmButtonText: '确定',
-                  cancelButtonText: '取消',
-                  type: 'warning'
-              }).then(() => {
-                  let bid = row.bid
-                  deleteAction('/kunze/brand/delBrand', {bid: bid}).then((res) => {
-                      if(res.code==200){
-                          this.$message({
-                              type: 'success',
-                              message: '删除成功!'
-                          });
-                          this.int()
-                      }else {
-                          this.$message.error('删除失败');
-                      }
-
-                  }).cache((err) => {
-                      this.$message.error('删除失败');
-                  })
-              })
-          },
-        int(){
-            getAction('/kunze/brand/qryBrandList',{pageSize:10}).then((res)=>{
-               this.tableData=res.result.list
-
-            })
-            getAction('/kunze/brand/qryBrandList',{pageSize:1}).then((res)=>{
-
-
-                this.total=res.result.pages
-            })
-        }
-      },
-      mounted() {
-          this.int()
       }
-  }
+    },
+    mounted(){
+      this.getAllCategories()
+    }
+  };
 </script>
 
 <style scoped>
-  .text {
-    font-size: 14px;
+  .modifyBtn{
+    margin-right: 10px;
   }
-
-  .item {
-    margin-bottom: 18px;
-  }
-
-  .clearfix:before,
-  .clearfix:after {
-    display: table;
-    content: "";
-  }
-  .clearfix:after {
-    clear: both
-  }
-
-  .box-card {
-    width:100%;
-
-  }
-  .flgl-btn{
-    margin-bottom: 20px;
-  }
-  /deep/.el-table td, .el-table th.is-leaf{
-    text-align: center;
-  }
-  /deep/.el-table--border th, .el-table__fixed-right-patch{
-    text-align: center;
-  }
-  /deep/.el-select{
-    width: 100%;
-  }
-   /deep/.el-upload {
-    border: 1px dashed #d9d9d9;
-    border-radius: 6px;
-    cursor: pointer;
-    position: relative;
-    overflow: hidden;
-  }
-   /deep/.el-upload:hover {
-    border-color: #409EFF;
-  }
-  .avatar-uploader-icon {
-    font-size: 28px;
-    color: #8c939d;
-    width: 178px;
-    height: 178px;
-    line-height: 178px;
-    text-align: center;
-  }
-  .avatar {
-    width: 178px;
-    height: 178px;
-    display: block;
+  .modifyBtn1{
+    margin-bottom: 10px;
   }
 </style>
