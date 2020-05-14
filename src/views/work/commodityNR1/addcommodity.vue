ee722c5a4127f19b7d64dbeb782e6ed22f84ac7e
niuniu13453581675 <2670709623@qq.com>
2020-05-14 17:58:11 +0800

    添加work

--- src/views/work/commodityNR1/addcommodity.vue
+++ src/views/work/commodityNR1/addcommodity.vue
@@ -1,70 +1,191 @@
 <template>
 <!--    新增商品-->
   <a-card title="新增商品">
+    <div style="width: 65%">
     <a-form-model
       ref="ruleForm"
       :model="form"
-      :rules="rules"
       :label-col="labelCol"
       :wrapper-col="wrapperCol"
     >
-      <a-form-model-item label="品牌" prop="brand">
+      <a-form-model-item label="标题" >
+        <a-input v-model="form.title" />
+      </a-form-model-item>
+      <a-form-model-item label="子标题" >
+        <a-input v-model="form.subTitle" />
+      </a-form-model-item>
+      <a-form-model-item label="品牌" >
         <a-select v-model="form.brand" placeholder="选择品牌">
           <a-select-option  v-for="v in brand" :value=v.bid :key="v.keys">
-            {{v.keys}}
+            {{v.bname}}
           </a-select-option>
         </a-select>
       </a-form-model-item>
-      <a-form-model-item label="分类" prop="flei">
+      <a-form-model-item label="分类" >
         <a-cascader
           :field-names="{ label: 'name', value: 'id', children: 'childrenList' }"
           :options="options"
-          placeholder="Please select"
+          placeholder="选择所属分类"
           @change="onChange"
         />
       </a-form-model-item>
+      <template v-if="dxuandatas.length!=0">
+        <a-form-model-item label="选择参数" >
+          <template v-for="v in dxuandatas">
+            <template v-for="x in v.params">
+              <template v-if="x.options!=''">
+                <a-row>
+                  <a-col :span="4">
+                    <span class="fenzu">{{x.k}}:</span>
+                  </a-col>
+                  <a-col :span="20">
+                    <a-radio-group  :name=x.k :v-model=x.value :options="x.options" :default-value="x.options[0]" @change="onChange2" />
+                  </a-col>
+                </a-row>
+                <br />
+              </template>
+            </template>
+          </template>
+        </a-form-model-item>
+      </template>
+      <a-form-model-item label="商品图片" >
+        <j-image-upload class="avatar-uploader" text="上传" v-model="fileList" ></j-image-upload>
+      </a-form-model-item>
+      <a-form-model-item label="销售价格" >
+        <a-input v-model="form.price" placeholder="单位为分" />
+      </a-form-model-item>
+      <a-form-model-item label="商品库存" >
+        <a-input v-model="form.stock" />
+      </a-form-model-item>
+      <a-form-model-item label="商品库存" >
+        <a-input v-model="form.stock" />
+      </a-form-model-item>
+      <a-form-model-item label="包装清单" >
+        <a-input v-model="form.packingList" />
+      </a-form-model-item>
+      <a-form-model-item label="售后服务" >
+        <j-editor v-model="form.afterService"/>
+      </a-form-model-item>
       <a-form-model-item :wrapper-col="{ span: 14, offset: 4 }">
         <a-button type="primary" @click="onSubmit">
-          Create
+          添加
         </a-button>
         <a-button style="margin-left: 10px;" @click="resetForm">
-          Reset
+          重置
         </a-button>
       </a-form-model-item>
     </a-form-model>
+    </div>
   </a-card>
 
 </template>
 
 <script>
   import {getAction} from '../../../api/manage'
+  import JImageUpload from '../../../components/jeecg/JImageUpload'
+  import JEditor from '@/components/jeecg/JEditor'
 
+  import Vue from 'vue'
   export default {
     name: 'addcommodity',
+    components: {
+      JImageUpload,
+      JEditor
+    },
       data(){
         return{
-          labelCol: { span: 2 },
-          wrapperCol: { span: 16 },
+          labelCol: { span: 4 },
+          wrapperCol: { span: 14 },
           other: '',
           form: {
             brand:'',
+            price:"",
+            stock:'',
+            description:'',
+            afterService:'',
+            packingList:'',
+            subTitle:'',
+            title:'',
           },
           brand:[],  //品牌的集合
           options:[],  //分类级联下拉
-          rules: {
-            brand: [{ required: true, message: 'Please select Activity zone', trigger: 'change' }],
-            flei: [{ required: true, message: 'Please select Activity zone', trigger: 'change' }],
-          },
+          dxuandatas:[],  //参数集合
+          ownSpec:{},
+          indexes:[],
+          index:[],
+          specTemplate:{},
+          specifications:[],
+          fileList:[]
         }
       },
       methods:{
-        onChange(value){
-          console.log(value)
-          getAction('/kunze/spec/specList',{categoryId:value[2]}).then((res)=>{
-            // let nn=res.result.specifications
+        //点击参数单选框
+        onChange2(e){
+          let val=e.target.value
+          let key=e.target.name
+          let that=this
+          for(let i in that.ownSpec){
+            if(key==i){
+              that.ownSpec[i]=val
+            }
+          }
+          that.index.forEach((e,y)=>{
+            if(e==key){
+              for(let i in that.specTemplate){
+                if(key==i){
+                  that.specTemplate[i].forEach((e,l)=>{
+                    if(e==val){
+                      that.indexes[y]=l
+                    }
+                  })
+                }
+              }
+            }
+          })
 
+        },
+        onChange(value){
+          let that=this
+          that.indexes=[]
+          that.ownSpec={}
+          that.index=[]
+          that.specTemplate={}
+          that.specifications=[]
+          getAction('/kunze/spec/specList',{categoryId:value[2]}).then((res)=>{
+            if(res.result==null){
+              that.dxuandatas=[]
+            }else {
+              that.dxuandatas=JSON.parse(res.result.specifications)
+              that.dxuandatas.forEach(e=>{
+
+                e.params.forEach((y,i)=>{
+                  y.value=''
+
+                  if(typeof y.options=='string'){
+                    y.options=y.options.split(',');
+                    if(y.options[0]==''){
+
+                    }else {
+                      // console.log(y.k,y.options[0])
+                      Vue.set(that.ownSpec,y.k,y.options[0])
+                      Vue.set(that.specTemplate,y.k,y.options)
+                      that.indexes.push(0)
+                      that.index.push(y.k)
+                      that.specifications.push({
+                        group:e.group,
+                        params:e.params
+                      })
+                    }
+
+                  }
+                })
+
+              })
+
+            }
 
           })
+          console.log(that.specifications)
         },
         onSubmit(){
 
@@ -94,5 +215,14 @@
 </script>
 
 <style scoped>
-
+  .fenzu{
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    -webkit-line-clamp: 1;
+    overflow: hidden;
+  }
+  .avatar-uploader > .ant-upload {
+    width:104px;
+    height:104px;
+  }
 </style>
